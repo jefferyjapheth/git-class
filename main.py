2fa18426d0c2ce9eba970e633abf7ee4153954ff
eeTiboah <etiboah@gmail.com>
2024-02-17 13:04:40 +0000

    add favorite food

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 
 name = input("What is your name?: ")
 year = int(input("What year were you born?: "))
+favorite_food = input("What is your favorite food?: ")
 
 age = 2024 - year
 
-print(f"Your name is {name} and you are {year} years old")
+print(f"Your name is {name} and you are {year} years old. Your favorite food is {favorite_food}")
+
